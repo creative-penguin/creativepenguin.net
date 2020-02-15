4ec50c9f5975d54fb9eaf3bafce962d5a23c61ca
Jim Jenkins <jimjenkins5@gmail.com>
2020-02-15 14:17:26 -0500

    chore: initial commit of site

---
+++ infrastructure/stacks/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { StaticSite, StaticSiteProps } from './static-site';
+import config = require('config');
+
+
+const app = new cdk.App();
+
+const staticSiteProps: StaticSiteProps = {
+   env: { account: config.get<string>('aws.accountID'), region: config.get<string>('aws.region') },
+};
+
+if (config.has('domain.name')) {
+   staticSiteProps.domainName = config.get<string>('domain.name');
+}
+
+if (config.has('domain.subdomains')) {
+   staticSiteProps.subdomains = config.get<string[]>('domain.subdomains');
+}
+
+new StaticSite(app, 'creativepenguin-net', staticSiteProps);
